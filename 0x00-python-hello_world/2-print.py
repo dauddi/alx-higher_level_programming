34cbcd3afb451225c7d3dbbc7c77782fd28525cf
Davis Okioma <kuntaytc@gmail.com>
2021-09-07 00:44:18 +0300

    2-print.py

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
+<<<<<<< HEAD
 print("\"Programming is like building a multilingual puzzle")
+=======
+
+print('"Programming is like building a multilingual puzzle');
+>>>>>>> 132114f (2-print.py)
