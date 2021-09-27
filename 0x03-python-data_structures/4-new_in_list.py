e2147347ed18f4c84dbbe29e2849e3f2d12f8c1b
Davis Okioma <kuntaytc@gmail.com>
2021-09-27 18:34:24 +0000

    13- is palindrome

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    my_list_copy = my_list
-    if idx < 0 or idx > len(my_list):
-        return my_list_copy
-    my_list_copy[idx] = element
-    return my_list_copy
+    copy = my_list.copy()
+    for x in my_list:
+        if idx == my_list.index(x):
+            copy[idx] = element
+            return (copy)
+        elif idx >= len(my_list):
+            return (copy)
+        elif idx < 0:
+            return (copy)
