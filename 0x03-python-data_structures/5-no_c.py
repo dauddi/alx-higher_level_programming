3b129157e7e651c7a75105a3de459c6ba46a3ea6
Davis Okioma <kuntaytc@gmail.com>
2021-09-27 18:40:46 +0000

    5- no c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,9 +2,7 @@
 def no_c(my_string):
     l = list(my_string)
     for x in l:
-        if x == 'c':
-            l.remove('c')
-        if x == 'C':
-            l.remove('C')
+        if x in 'cC':
+            l.remove(x)
     my_string = "".join(l)
     return my_string
